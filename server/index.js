062633fc2c117a1b5b90d0e12178e4308d033e5f
herizo fanomzana <herizo.ratefiarison@gmail.com>
2023-01-09 17:50:42 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import dotenv from 'dotenv';
+
+
+import clientRoutes from './routes/clients.js';
+import generalRoutes from './routes/generals.js';
+import managementRoutes from './routes/managements.js';
+import campagnesRoutes from './routes/campagnes.js';
+
+
+
+/** CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cors());
+
+
+/** ROUTES */
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/campagnes", campagnesRoutes);
+
+
+/** MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 9000;
+
+mongoose.connect(process.env.MONGO_URL,{
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((err) => console.log(`${err} Server not connected`));
